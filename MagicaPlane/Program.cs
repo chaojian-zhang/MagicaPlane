dbe7b5fdb2499184f2f0f83c42899e7192df6ea6
Charles Zhang <szinubuntu@gmail.com>
2019-12-17 16:37:53 -0500

    Add project files.

---
+++ MagicaPlane/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace MagicaPlane
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Check null argument
+            if(args.Length == 0)
+                Console.WriteLine("Missing argument for input folder!");
+            // Check folder existence
+            else if(!Directory.Exists(args[0]))
+                Console.WriteLine("Input folder doesn't exist!");
+            else
+            {
+                string dir = args[0];
+                var layers = Directory.EnumerateFiles(dir)
+                    .Where(f => Path.GetFileNameWithoutExtension(f) != "material" && int.TryParse(Path.GetFileNameWithoutExtension(f), out _))
+                    .OrderBy(f => int.Parse(Path.GetFileNameWithoutExtension(f)))
+                    .ToArray();
+                string material = Directory.EnumerateFiles(dir).Single(f => Path.GetFileNameWithoutExtension(f) == "material");
+                // Determine dimension
+                int height = layers.Length;
+                int rows, columns;
+                GetDimensions(File.ReadAllLines(layers.First()), out rows, out columns);
+                // Read material definition
+                Dictionary<string, int> materials = ReadMaterial(material);
+                // Build index list
+                StringBuilder builder = new StringBuilder();
+                // Load layers
+                for (int z = 0; z < layers.Length; z++)
+                {
+                    string[] csvLines = File.ReadAllLines(layers[z]);
+                    for (int r = 0; r < csvLines.Length; r++)
+                    {
+                        string line = csvLines[r];
+                        string[] cols = line.Split(',');
+                        for (int c = 0; c < cols.Length; c++)
+                        {
+                            string col = cols[c];
+                            int index = materials[col];
+                            // Set volume value
+                            builder.Append($"{index},"); // Order: col, row, height
+                        }
+                    }
+                }
+                // remove additional comma
+                builder.Remove(builder.Length - 1, 1);
+                // Generate result
+                string shaderPath = Path.Combine(dir, "shader.txt");
+                string shader = ReadResource("Template.txt");
+                shader = shader.Replace("{{TotalGridSize}}", $"{rows * columns * layers.Length}");
+                shader = shader.Replace("{{IndexList}}", builder.ToString());
+                shader = shader.Replace("{{Width}}", $"{columns.ToString()}/*x*/");
+                shader = shader.Replace("{{Length}}", $"{rows.ToString()}/*y*/");
+                shader = shader.Replace("{{Height}}", $"{layers.Length.ToString()}/*z*/");
+                File.WriteAllText(shaderPath, shader);
+            }
+        }
+
+        private static Dictionary<string, int> ReadMaterial(string materialPath)
+        {
+            Dictionary<string, int> materials = new Dictionary<string, int>();
+            string[] lines = File.ReadAllLines(materialPath);
+            foreach (var line in lines)
+            {
+                string[] cols = line.Split(',');
+                string name = cols[0];
+                int index = Convert.ToInt32(cols[1]);
+                materials[name] = index;
+            }
+            return materials;
+        }
+
+        static void GetDimensions(string[] csvLines, out int rows, out int columns)
+        {
+            rows = csvLines.Length;
+            columns = csvLines[0].Split(',').Count();
+        }
+
+        static string ReadResource(string name)
+        {
+            // Determine path
+            var assembly = Assembly.GetExecutingAssembly();
+            string resourcePath = name;
+            // Format: "{Namespace}.{Folder}.{filename}.{Extension}"
+            if (!name.StartsWith(nameof(MagicaPlane)))
+            {
+                resourcePath = assembly.GetManifestResourceNames()
+                    .Single(str => str.EndsWith(name));
+            }
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourcePath))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,73 @@
+# Motivation
+
+1. (Cause) MagicaVoxel is great for **visualization and rendering**, but I don't quite enjoy how its editing works. 
+2. (Reason) One might try to use custom editor then generate directly to `.vox` format, that doesn't work, or generate directly to `.schematic` format, that doesn't work as well. One might expect to use only **MagicaVoxel View** - but none of the formats it claims to support seems to have and well-defined specification I can find.
+3. (Alternative) There is **FileToVox** project, which unfortunately contains too many irrelavant code for my purpose.
+4. (Definition) This tool (was originally conceived to) converts sequence of same-dimension `.png` into a MagicaVoxel shader, which when executed can have the effect of generating a corresponding `.vox` object - thus allowing a **plane based editing** experience.
+	* But that is troublesome and require additional photo editing tools and coloring is troublesome, so instead we use *headless* `.csv` based approach.
+5. (Feature) Naturally, to workaround the 126x126x126 size limit, we will support breaking the volumes down into seperate pieces.
+6. (Future) Ideally we support better ways to exploit MagicaVoxel's `.vox` file format or MagicaVoxel Viewer more directly (e.g. support its *sparse oct tree* formats).
+7. (Future) I just don't like the way MagicaVoxel's documentation, specification, and format design, ideally I would make my own alternative solution, but to compare with MagicaVoxel will require too much work.
+
+# Remaining Problems
+
+1. I don't even know the language format for **shader** file - does it support global variables, arrays, multi-dimension arrays? GL Shading language, per [here](https://github.com/CodingEric/Erics-MagicaVoxel-Shaders)?
+
+# Usage
+
+1. Draw your desired shapes in a sequence of *headless* `.csv` files to represent same-dimension layers for final voxel design, filling each cell a **target material name**; Those sequence files shall be named with numerical filenames, and will be ordered from lowest as bottom layers to highest as top layers, exact numerical values don't matter, only the relative order matters; See **sample layer** in **Reference** below.
+2. Prepare a sepearte *headless* `.csv` file named `material.csv` which have two columns: **name**, and **value**, where "value" maps to index in the pallete (from `1`-`255`); See **sample material** in **Reference** below.
+3. Put all those files in a common folder;
+4. Execute MP and pass in folder path;
+5. MP will generate a shader file for you to consume: you need to prepare the corresponding **cube dimension** and **clean the cube** before executing the shader.
+
+# Formats for Material Value
+
+Index of Color palette as you will use for Material property. Notice if a grid cell is empty, you need to specify null explicitly with index 0 (see example below).
+
+# Implementation
+
+The basic mechanism for this is simple: a large const array will be defined for each `vec3` position of the volume, then inside `map()` function we will just sample from it.
+
+Example:
+
+```glsl
+const float volume[5] = float[5](1, 2, 3, 4, 5);
+
+float map(vec3 v) {
+	int index = int(mod(v.x, 5));
+	return volume[index];
+}
+```
+
+# Reference
+
+**Sample Layer**
+
+Something like `1.csv` and `2.csv` defines a `5 (row) x 3(col) x 2 (height)` volume (which corresponds to a total of `5x3x2=30` values):
+
+```csv
+ground,ground,ground
+ground,ground,ground
+ground,ground,ground
+ground,ground,ground
+ground,ground,ground
+```
+
+```csv
+empty,ground,empty
+ground,ground,ground
+ground,light,ground
+ground,ground,ground
+empty,ground,empty
+```
+
+**Sample Material**
+
+`material.csv`:
+
+```csv
+empty, 0
+ground,1
+light,5
+```
