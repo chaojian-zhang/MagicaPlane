a23b1c294fc2a16b58c0f4bde0e2430616365912
Charles Zhang <szinubuntu@gmail.com>
2019-12-17 18:47:34 -0500

    Add example, add utility command to initialize folder, add exception handling, add error message, refactor and organize code, make material names case-insensitive

--- MagicaPlane/Program.cs
+++ MagicaPlane/Program.cs
@@ -13,55 +13,113 @@ namespace MagicaPlane
         {
             // Check null argument
             if(args.Length == 0)
-                Console.WriteLine("Missing argument for input folder!");
+                Console.WriteLine("Missing argument for input folder!\n" +
+                    "Accepted commands: MagicaPlane inputFolder [rows cols height]\n" +
+                    "Use optional [rows cols height] arguments to initialize an EMPTY folder.");
             // Check folder existence
             else if(!Directory.Exists(args[0]))
                 Console.WriteLine("Input folder doesn't exist!");
+            // Initialize command
+            else if (args.Length == 4)
+            {                
+                string dir = Path.GetFullPath(args[0]);
+                // Check empty
+                if (Directory.GetFiles(dir).Length != 0)
+                    Console.WriteLine("Input folder not empty!");
+                else
+                    InitializeFolder(Convert.ToInt32(args[1]), Convert.ToInt32(args[2]), Convert.ToInt32(args[3]), dir);
+            }
             else
             {
-                string dir = args[0];
-                var layers = Directory.EnumerateFiles(dir)
-                    .Where(f => Path.GetFileNameWithoutExtension(f) != "material" && int.TryParse(Path.GetFileNameWithoutExtension(f), out _))
-                    .OrderBy(f => int.Parse(Path.GetFileNameWithoutExtension(f)))
-                    .ToArray();
-                string material = Directory.EnumerateFiles(dir).Single(f => Path.GetFileNameWithoutExtension(f) == "material");
-                // Determine dimension
-                int height = layers.Length;
-                int rows, columns;
-                GetDimensions(File.ReadAllLines(layers.First()), out rows, out columns);
-                // Read material definition
-                Dictionary<string, int> materials = ReadMaterial(material);
-                // Build index list
+                string dir = Path.GetFullPath(args[0]);
+                // Check empty
+                if(Directory.GetFiles(dir).Length == 0)
+                    Console.WriteLine("Input folder is empty.");
+                else if(!Directory.GetFiles(dir).Any(f => Path.GetFileName(f) == "material.csv"))
+                    Console.WriteLine("Input folder missing material definition.");
+                else
+                    ParseFolder(dir);
+            }
+        }
+
+        private static void InitializeFolder(int rows, int cols, int heights, string dir)
+        {
+            // Write layers
+            for (int f = 0; f < heights; f++)
+            {
                 StringBuilder builder = new StringBuilder();
-                // Load layers
-                for (int z = 0; z < layers.Length; z++)
+                for (int r = 0; r < rows; r++)
                 {
-                    string[] csvLines = File.ReadAllLines(layers[z]);
-                    for (int r = 0; r < csvLines.Length; r++)
+                    for (int c = 0; c < cols; c++)
+                        builder.Append("empty,");
+                    // Remove trailing additional comma
+                    builder.Remove(builder.Length - 1, 1);
+                    // Append new line to start a new row
+                    builder.Append('\n');
+                }
+                // Remove trailing empty line
+                builder.Remove(builder.Length - 1, 1);
+                // Write to file
+                File.WriteAllText(Path.Combine(dir, $"{f + 1}.csv"), builder.ToString());
+            }
+            // Write material
+            File.WriteAllText(Path.Combine(dir, "material.csv"), "empty, 0\ncustom, 5");
+        }
+
+        private static void ParseFolder(string dir)
+        {
+            var layers = Directory.EnumerateFiles(dir)
+                                .Where(f => Path.GetFileNameWithoutExtension(f) != "material" && int.TryParse(Path.GetFileNameWithoutExtension(f), out _))
+                                .OrderBy(f => int.Parse(Path.GetFileNameWithoutExtension(f)))
+                                .ToArray();
+            string material = Directory.EnumerateFiles(dir).Single(f => Path.GetFileNameWithoutExtension(f) == "material");
+            // Determine dimension
+            int height = layers.Length;
+            int rows, columns;
+            GetDimensions(File.ReadAllLines(layers.First()), out rows, out columns);
+            // Read material definition
+            Dictionary<string, int> materials = ReadMaterial(material);
+            // Build index list
+            StringBuilder builder = new StringBuilder();
+            // Load layers
+            for (int z = 0; z < layers.Length; z++)
+            {
+                string[] csvLines = File.ReadAllLines(layers[z]);
+                for (int r = 0; r < csvLines.Length; r++)
+                {
+                    string line = csvLines[r];
+                    // Skip empty
+                    if (string.IsNullOrWhiteSpace(line))
+                        continue;
+                    // Get columns
+                    string[] cols = line.Split(',');
+                    for (int c = 0; c < cols.Length; c++)
                     {
-                        string line = csvLines[r];
-                        string[] cols = line.Split(',');
-                        for (int c = 0; c < cols.Length; c++)
+                        string col = cols[c].ToLower(); // Material is case insensitive
+                        if(!materials.ContainsKey(col))
                         {
-                            string col = cols[c];
-                            int index = materials[col];
-                            // Set volume value
-                            builder.Append($"{index},"); // Order: col, row, height
+                            string replacement = materials.First().Key;
+                            Console.WriteLine($"Missing material definition `{col}` on line ({r+1}), col ({c+1}) of file `{layers[z]}`, " +
+                                $"using `{replacement}` instead.");
+                            col = replacement;
                         }
+                        int index = materials[col];
+                        // Set volume value
+                        builder.Append($"{index},"); // Order: col, row, height
                     }
                 }
-                // remove additional comma
-                builder.Remove(builder.Length - 1, 1);
-                // Generate result
-                string shaderPath = Path.Combine(dir, "shader.txt");
-                string shader = ReadResource("Template.txt");
-                shader = shader.Replace("{{TotalGridSize}}", $"{rows * columns * layers.Length}");
-                shader = shader.Replace("{{IndexList}}", builder.ToString());
-                shader = shader.Replace("{{Width}}", $"{columns.ToString()}/*x*/");
-                shader = shader.Replace("{{Length}}", $"{rows.ToString()}/*y*/");
-                shader = shader.Replace("{{Height}}", $"{layers.Length.ToString()}/*z*/");
-                File.WriteAllText(shaderPath, shader);
             }
+            // remove additional comma
+            builder.Remove(builder.Length - 1, 1);
+            // Generate result
+            string shaderPath = Path.Combine(dir, $"{Path.GetFileName(dir)}.txt");  // Name shader as name of folder
+            string shader = ReadResource("Template.txt");
+            shader = shader.Replace("{{TotalGridSize}}", $"{rows * columns * layers.Length}");
+            shader = shader.Replace("{{IndexList}}", builder.ToString());
+            shader = shader.Replace("{{Width}}", $"{columns.ToString()}/*x*/");
+            shader = shader.Replace("{{Length}}", $"{rows.ToString()}/*y*/");
+            shader = shader.Replace("{{Height}}", $"{layers.Length.ToString()}/*z*/");
+            File.WriteAllText(shaderPath, shader);
         }
 
         private static Dictionary<string, int> ReadMaterial(string materialPath)
@@ -71,7 +129,7 @@ namespace MagicaPlane
             foreach (var line in lines)
             {
                 string[] cols = line.Split(',');
-                string name = cols[0];
+                string name = cols[0].ToLower();    // Take lower case
                 int index = Convert.ToInt32(cols[1]);
                 materials[name] = index;
             }
